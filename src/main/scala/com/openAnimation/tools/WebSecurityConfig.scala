57dfd3d330524f730b436bd7b3d3f9872b35f342
Liam-Robertson <liamjohnrobertson@gmail.com>
2022-05-10 21:22:40 +0100

    it works, took out spring security for now. Doesn't work with firewall

--- src/main/scala/com/openAnimation/tools/WebSecurityConfig.scala
+++ src/main/scala/com/openAnimation/tools/WebSecurityConfig.scala
@@ -1,23 +1,43 @@
-package com.openAnimation.tools
-
-import org.springframework.context.annotation.{Bean, Configuration}
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.{EnableWebSecurity, WebSecurityConfigurerAdapter};
-
-@EnableWebSecurity
-@Configuration
-  class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Override
-  override def configure(http: HttpSecurity) {
-
-        http
-            .authorizeRequests()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .httpBasic();
-  }
-
-
-}
+//package com.openAnimation.tools
+//
+//import org.springframework.context.annotation.{Bean, Configuration}
+//import org.springframework.security.config.annotation.web.builders.{HttpSecurity, WebSecurity}
+//import org.springframework.security.config.annotation.web.configuration.{EnableWebSecurity, WebSecurityConfigurerAdapter}
+//import org.springframework.security.web.firewall.{DefaultHttpFirewall, HttpFirewall, HttpStatusRequestRejectedHandler, RequestRejectedHandler, StrictHttpFirewall};
+//
+//@EnableWebSecurity
+//@Configuration
+//  class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//  @Override
+//  override def configure(http: HttpSecurity) {
+//
+//        http
+//          .authorizeRequests()
+//          .anyRequest()
+//          .authenticated()
+//          .and()
+//          .httpBasic();
+//        http.cors();
+//  }
+//
+//  @Bean
+//  def allowUrlEncodedSlashHttpFirewall(): HttpFirewall = {
+//    //    val firewall: StrictHttpFirewall = new StrictHttpFirewall()
+//    //    firewall.setAllowUrlEncodedSlash(true)
+//    //    firewall
+//        new DefaultHttpFirewall
+//  }
+//
+//  @Override
+//  override def configure(web: WebSecurity) {
+//    super.configure(web);
+//    web.httpFirewall(allowUrlEncodedSlashHttpFirewall())
+//  }
+//
+//  @Bean
+//  def requestRejectedHandler(): RequestRejectedHandler = {
+//    new HttpStatusRequestRejectedHandler()
+//  }
+//
+//}
